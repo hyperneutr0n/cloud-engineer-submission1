a721032bd94e86585d87e7e84a0d6b5c0b2cdb8c
hyperneutr0n <randylemuel21@gmail.com>
2024-11-19 18:49:34 +0700

    Updated paths

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const path = require("path");
 const app = express();
 
 // Serve static files from the "public" folder
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 // Home route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 // Start the server
